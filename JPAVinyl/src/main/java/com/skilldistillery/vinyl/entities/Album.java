d4b716bd977042128ee79d4fe20cd1d67cec85eb
paul-chaffin <pechaffin@pm.me>
2022-05-13 18:18:11 -0400

    Configure hibernate / JUnit test Album entity

---
+++ JPAVinyl/src/main/java/com/skilldistillery/vinyl/entities/Album.java
@@ -0,0 +1,42 @@
+package com.skilldistillery.vinyl.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Album {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+
+	public Album() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "Album [id=" + id + ", title=" + title + "]";
+	}
+
+}
